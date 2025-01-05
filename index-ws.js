6de8889e9a2627a5680e3e864c884eaf9aee2f0b
Amit Ranjan <connectdevamit@gmail.com>
2025-01-05 19:47:53 +0000

    Added the ws connection

--- index-ws.js
+++ index-ws.js
@@ -10,3 +10,29 @@ app.get('/', function(req, res){
 server.on('request', app);
 server.listen(3000, function() { console.log('server started on port 3000'); });
 
+/** Begin Websocket */
+const WebsocketServer = require('ws').Server;
+
+const wss = new WebsocketServer({server});
+
+wss.on('connection', function connection(ws){
+	const totalClients = wss.clients.size;
+	console.log('Clients connected', totalClients);
+	wss.broadcast(`Current visitors: ${numClients}`);
+	
+	if(ws.readyState === ws.OPEN){
+		ws.send('Welcome to my server');
+	}
+
+	ws.on('close', function close(){
+		wss.broadcast(`Current visitors: ${numClients}`);
+		console.log('A client has disconnected');
+	});
+
+});
+
+wss.broadcast = function broadcast(data){
+	wss.clients.forEach(function each(client){
+		client.send(data);
+	});
+}
